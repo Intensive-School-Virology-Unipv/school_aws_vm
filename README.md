c293b493966d139a696b9ea76c6c54e606b2aad3
Francesco <francesco@lescai.eu>
2021-06-24 16:24:09 +0200

    update readme file with instructions

--- README.md
+++
@@ -1,2 +0,0 @@
-# school_aws_vm
-setup and management of an AWS virtual machine for teaching
--- terraform_code/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = ["${aws_instance.viro-vm.*.id}"]
-}
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = ["${aws_instance.viro-vm.*.public_ip}"]
-}
