a10b9ead067135359d3f2c43a16cbc06a56750fb
Francesco <francesco@lescai.eu>
2021-06-25 14:00:04 +0200

    updated AMI reference

--- terraform_code/viro_vm_multi.tf
+++ terraform_code/viro_vm_multi.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "viro-vm" {
     count = var.instance_count
-    ami = "ami-09f0086e65f02e893"
+    ami = "ami-0a61a97321eff5390"
     instance_type = var.instance_type
     security_groups = [ "training-test-01" ]
 
